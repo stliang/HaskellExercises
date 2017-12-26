25b8689204134fe78cedc819516d8c239cc0993e
stliang <stliang@gmail.com>
2017-12-26 09:38:18 -0800

    AST exercise on page 702
    Eval and print expressions

---
+++ FirstPrinciple/702.hs
@@ -0,0 +1,18 @@
+data Expr
+  = Lit Integer
+  | Add Expr
+        Expr
+
+eval :: Expr -> Integer
+eval (Lit n) = n
+eval (Add e1 e2) = eval e1 + eval e2
+
+printExpr :: Expr -> String
+printExpr (Lit n) = show n
+printExpr (Add e1 e2) = printExpr e1 ++ " + " ++ printExpr e2
+
+a1 = Add (Lit 9001) (Lit 1)
+
+a2 = Add a1 (Lit 20001)
+
+a3 = Add (Lit 1) a2
