f9a634f8cf33362e063372dbb1eca4ac65fe4b57
stliang <stliang@gmail.com>
2017-12-26 09:15:00 -0800

    694 to 696 solutions
    Exercise solutions to page 694 to 696 concerning operations on list

--- FirstPrinciple/694.hs
+++ FirstPrinciple/694.hs
@@ -16,3 +16,17 @@ capitalizeWords inputString = zip old new
     capHead = fmap (\(x:xs) -> toUpper x : xs)
     old = words inputString
     new = capHead old
+
+capitalizeParagraph :: String -> String
+capitalizeParagraph inputString = concat $ go (capitalizeWords inputString) True False
+  where
+    period :: [Char] -> Bool
+    period [] = False
+    period xs = (head $ reverse xs) == '.'
+    go :: [(String, String)] -> Bool -> Bool -> [String]
+    go [] _ _ = []
+    go (x:[]) _ _ = [fst x]
+    go (x:xs) fstChar aftPeriod =
+      case fstChar || aftPeriod of
+        True -> snd x : " " : go xs False (period (snd x))
+        _ -> fst x : " " : go xs False (period (fst x))
