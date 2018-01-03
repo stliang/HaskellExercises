8d5dac25dd6e296c968a2ffe02bd539205c0c1b7
stliang <stliang@gmail.com>
2018-01-02 16:01:45 -0800

    added game play scenario in Main
    In main, added all ships to grid and attack all positions of all ships.
     Print out the ship layout and condition of the game after all attacks.

--- battleship/app/Main.hs
+++ battleship/app/Main.hs
@@ -5,7 +5,8 @@ import Types
 
 main :: IO ()
 main = do
-  let scene = Scene (mkShipGrid) []
+  let scene = Scene mkShipGrid []
+      -- Declare ship placements
       args =
         [ ((2, 2), TailRight, Carrier)
         , ((2, 8), TailDown, Destroyer)
@@ -13,7 +14,30 @@ main = do
         , ((7, 9), TailUp, Cruiser)
         , ((7, 4), TailDown, Battleship)
         ]
-      eitherScene = foldr (\(c, d, s) (Right scene') -> placeShip c d s scene') (Right scene) args
-  case eitherScene of
-    (Right scene'') -> showGrid $ myShipGrid scene''
-    (Left e) -> putStrLn $ show e
+      -- Declare ship targets
+      carrirerTargetList = fmap mkTargetCoordinate [(2, 2), (2, 3), (2, 4), (2, 5), (2, 6)]
+      destroyerTargetList = fmap mkTargetCoordinate [(2, 8), (3, 8)]
+      submarineTargetList = fmap mkTargetCoordinate [(5, 3), (5, 4), (5, 5)]
+      battleshipTargetList = fmap mkTargetCoordinate [(7, 4), (8, 4), (9, 4), (10, 4)]
+      cruiserTargetList = fmap mkTargetCoordinate [(5, 9), (6, 9), (7, 9)]
+      allShipTargets =
+        carrirerTargetList ++
+        destroyerTargetList ++ submarineTargetList ++ battleshipTargetList ++ cruiserTargetList
+      -- Place all ships in a scene
+      eScene = foldr (\(c, d, s) (Right scene') -> placeShip c d s scene') (Right scene) args
+      -- Attack all ships
+      eCond = do
+        scene'' <- eScene
+        let sGrid = myShipGrid scene''
+            tGrid = mkTargetingGrid
+            state = State tGrid sGrid Playing
+            state' = foldr (\x acc -> run x acc) state allShipTargets
+        return $ condition state'
+  -- Print out ship grid
+  case eScene of
+    (Right s) -> showGrid $ myShipGrid s
+    (Left e) -> putStrLn $ "error: " ++ (show e)
+  -- Print out condition of the game
+  case eCond of
+    (Right c) -> putStrLn (show c)
+    (Left e) -> putStrLn $ "error: " ++ (show e)
