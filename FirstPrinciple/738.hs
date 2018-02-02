ae160a10ac7299fd679e40bf499095c14f4ed29f
stliang <stliang@gmail.com>
2018-02-02 14:42:05 -0800

    Solution to page 738

---
+++ FirstPrinciple/738.hs
@@ -0,0 +1,38 @@
+isJust :: Maybe a -> Bool
+isJust (Just _) = True
+isJust _ = False
+
+isNothing :: Maybe a -> Bool
+isNothing Nothing = True
+isNothing _ = False
+
+mayybee :: (a -> b) -> b -> Maybe a -> b
+mayybee f _ (Just y) = f y
+mayybee f x Nothing = x
+
+fromMaybe :: a -> Maybe a -> a
+fromMaybe = mayybee id
+
+listToMaybe :: [a] -> Maybe a
+listToMaybe [] = Nothing
+listToMaybe (x:ys) = Just x
+
+maybeToList :: Maybe a -> [a]
+maybeToList Nothing = []
+maybeToList (Just x) = [x]
+
+catMaybes :: [Maybe a] -> [a]
+catMaybes [] = []
+catMaybes (x:xs) = maybeToList x ++ (catMaybes xs)
+
+flipMaybe :: [Maybe a] -> Maybe [a]
+flipMaybe [] = Nothing
+flipMaybe xs =
+  if (all (\x -> notNothing x) xs)
+    then Just $ catMaybes xs
+    else Nothing
+  where
+    notNothing x =
+      case x of
+        Just _ -> True
+        _ -> False
