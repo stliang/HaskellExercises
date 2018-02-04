43a82e30c64f0b1ffe811aa7aafb24a1c84dda84
stliang <stliang@gmail.com>
2018-02-03 20:50:20 -0800

    Solution to Maybe and Either exercises

--- FirstPrinciple/738.hs
+++ FirstPrinciple/738.hs
@@ -25,9 +25,47 @@ catMaybes :: [Maybe a] -> [a]
 catMaybes [] = []
 catMaybes (x:xs) = maybeToList x ++ (catMaybes xs)
 
-flipMaybe :: [Maybe a] -> Maybe [a]
+flipMaybe
+  :: (Eq a)
+  => [Maybe a] -> Maybe [a]
 flipMaybe [] = Nothing
 flipMaybe xs =
   if (Nothing `elem` xs)
     then Nothing
     else Just $ catMaybes xs
+
+leftToList :: Either a b -> [a]
+leftToList (Left x) = [x]
+leftToList (Right _) = []
+
+lefts' :: [Either a b] -> [a]
+lefts' [] = []
+lefts' (x:xs) = leftToList x ++ lefts' xs
+
+lefts'' :: [Either a b] -> [a]
+lefts'' = foldr (\x y -> leftToList x ++ y) []
+
+rightToList :: Either a b -> [b]
+rightToList (Left _) = []
+rightToList (Right x) = [x]
+
+right' :: [Either a b] -> [b]
+right' = foldr (\x y -> rightToList x ++ y) []
+
+partitionEithers' :: [Either a b] -> ([a], [b])
+partitionEithers' xs = (lefts, rights)
+  where
+    lefts = lefts'' xs
+    rights = right' xs
+
+eitherMaybe' :: (a -> c) -> Either a b -> Maybe c
+eitherMaybe' f (Left x) = Just $ f x
+eitherMaybe' _ _ = Nothing
+
+either' :: (a -> c) -> (b -> c) -> Either a b -> c
+either' l _ (Left x) = l x
+either' _ r (Right x) = r x
+
+eitherMaybe'' :: (b -> c) -> Either a b -> Maybe c
+eitherMaybe'' _ (Left _) = Nothing
+eitherMaybe'' f (Right x) = either' Just Just (Right $ f x)
